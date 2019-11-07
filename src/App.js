93133ed432c71735e24912e09fe49ca31253c9ed
Cyndi <31037969+CyndiJia@users.noreply.github.com>
2019-11-07 19:54:53 +0800

    lightup-fixed

--- src/App.js
+++ src/App.js
@@ -2,150 +2,128 @@ import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
+
+//C Major: CEG; D Major: DFshA G Major: GBD A Major: ACshE
 let Keys = [
   {
     id: 'white',
     key: "C",
-    major: 'CEG',
-    state: 'pressed'
+    major: 'CEG'
   },
   {
     id: 'black1',
-    key: "Csh",
-    major: 'CEG',
-    state: ''
+    key: "LCSH",
+    major: 'ACshE'
   },
   {
     id: 'white',
     key: "D",
-    major: 'D',
-    state: ''
+    major: 'D'
   },
   {
     id: 'black2',
-    key: "Dsh",
-    major: 'CEG',
-    state: ''
+    key: "LDSH",
+    major: 'CEG'
   },
   {
     id: 'white',
     key: "E",
-    major: 'CEG',
-    state: 'pressed'
+    major: 'CEG'
   },
   {
     id: 'white',
     key: "F",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'black3',
     key: "Fsh",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
     key: "G",
-    major: 'CEG',
-    state: "pressed"
+    major: 'CEG'
   },
   {
     id: 'black4',
     key: "Gsh",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
     key: "A",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'black5',
     key: "Ash",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
     key: "B",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
-    key: "CC",
-    major: 'D',
-    state: ""
+    key: "C",
+    major: 'D'
   },
   {
     id: 'black6',
     key: "CCsh",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
-    key: "DD",
-    major: 'D',
-    state: ""
+    key: "D",
+    major: 'D'
   },
   {
     id: 'black7',
     key: "DDsh",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
-    key: "EE",
-    major: 'D',
-    state: ""
+    key: "E",
+    major: 'D'
   },
   {
     id: 'white',
-    key: "FF",
-    major: 'D',
-    state: ""
+    key: "F",
+    major: 'D'
   },
   {
     id: 'black8',
     key: "FFsh",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
-    key: "GG",
-    major: 'D',
-    state: ""
+    key: "G",
+    major: 'D'
   },
   {
     id: 'black9',
     key: "GGsh",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
-    key: "AA",
-    major: 'D',
-    state: ""
+    key: "A",
+    major: 'D'
   },
   {
     id: 'black10',
     key: "AAsh",
-    major: 'D',
-    state: ""
+    major: 'D'
   },
   {
     id: 'white',
-    key: "BB",
-    major: 'D',
-    state: ""
+    key: "B",
+    major: 'D'
   }
 ]
 
@@ -158,38 +136,30 @@ function App() {
 }
 
 function Keyboard({ data }) {
-  const [buttonPressed, setPressed] = useState(false);//initial value of useState; Value that changes the use state
-  const [note,setNote] = useState(false);
+  const [note,setNote] = useState('');
+  const [majorSelected,setMajor] = useState('');
   
   function CMajor() {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].major);
-      if (data[i].major === 'CEG') {
-        setPressed(true);
-        setTimeout(back, 2000);
-      }
-    }
+    setMajor('CEG');
+    setTimeout(back, 2000);
   }
 
   function GMajor() {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].major);
-      if (data[i].major === 'D') {
-        setPressed(true);
-        setTimeout(back, 2000);
-      }
-    }
+    setMajor('GBD');
+    setTimeout(back, 2000);
   }
 
-  console.log(buttonPressed);
   function back() {
-    setPressed(false);
+    //setPressed(false);
+    setMajor('');
+    setNote('');
   }
 
   function handleKeyPress(event) {
-    if(event.key === 'c') {
+    console.log(event.target.value);
+    if(event.target.value !== '') {
       console.log('hi');
-      setNote(true);
+      setNote(event.key);
       setTimeout(back, 2000);
     }
   }
@@ -199,15 +169,15 @@ function Keyboard({ data }) {
       <div className="main">
         <div className="piano">
           <div className="keys">
-            {data.map(d => { return <Key id={d.id} x={d.x} y={d.y} key={d.key} major={d.major} noteEntered = {note} keyPressed={buttonPressed && d.state}></Key> })};
+            {data.map(d => { return <Key id={d.id} x={d.x} y={d.y} key={d.key} major={d.major} noteEntered={note.toUpperCase() === d.key} keyPressed={majorSelected.includes(d.key)}></Key> })}
           </div>
         </div>
         <div className="enterNote">
-          <input type="text" onKeyPress={handleKeyPress} />
+          <input type="text" onKeyUp={handleKeyPress} />
         </div>
         <div id="buttons">
           <button type="button" name="button" id="c" onClick={CMajor}>C Major</button>
-          <button type="button" name="button" id="c" onClick={GMajor}>G Major</button>
+          <button type="button" name="button" id="g" onClick={GMajor}>G Major</button>
         </div>
       </div>
     </div>
@@ -215,8 +185,9 @@ function Keyboard({ data }) {
   
 }
 
-function Key({ id, keyPressed, key, major }) {
-  return <div id={id} major={major} className={keyPressed ? "pressed" : ""}></div>
+
+function Key({ id, keyPressed, major,noteEntered }) {
+  return <div id={id} major={major} className={(keyPressed ? "pressed" : "") + " "+(noteEntered ? 'note':"")}></div>
 
 }
 
